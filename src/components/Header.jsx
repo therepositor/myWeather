4cb1fd5e52cfbbe669ee373a74d64c94ad4105f2
Joseph <joseph.oguntebi@yahoo.com>
2022-01-31 10:06:13 +0100

    allWarningsCleared

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCity } from '@fortawesome/free-solid-svg-icons'
+// import { Link } from 'react-router-dom';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faCity } from '@fortawesome/free-solid-svg-icons'
 const Header = ({onClick, onChange}) => {
   return (
     <header>
